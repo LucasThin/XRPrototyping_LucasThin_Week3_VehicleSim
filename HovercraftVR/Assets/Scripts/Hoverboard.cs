4d1c8b298fbe1b7ecf90fe68225546744cd53b22
Lucaswang8 <hi@madebylucas.co>
2022-09-14 15:13:27 -0700

    Got basic movements in. Got city scene in

--- HovercraftVR/Assets/Scripts/Hoverboard.cs
+++ HovercraftVR/Assets/Scripts/Hoverboard.cs
@@ -3,39 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Hoverboard : MonoBehaviour
 {
     [SerializeField] private GameObject _hoverboard;
+    [SerializeField] private Transform _forwardSource;
+    [SerializeField] private Transform _mainCamera;
+    [SerializeField] private GameObject _XROrigin;
+    [SerializeField] private GameObject _Avatar;
     [SerializeField] private InputActionReference _hoverBoardActivate;
+    [SerializeField] private InputActionReference _HoverUpDown;
+    [SerializeField] private ActionBasedContinuousMoveProvider _XROriginMove;
+    [SerializeField] private ActionBasedContinuousTurnProvider _XROriginTurn;
 
     [SerializeField] private Transform[] _bones = new Transform[7];
     [SerializeField] private Transform[] _skatePose = new Transform[7];
     [SerializeField] private Transform[] _standPose = new Transform[7];
-    
-   /* //Save skating poses
-    [SerializeField] private Transform _back;
-    [SerializeField] private Transform _leftFoot;
-    [SerializeField] private Transform _leftShin;
-    [SerializeField] private Transform _leftThigh;
-    [SerializeField] private Transform _rightFoot;
-    [SerializeField] private Transform _rightShin;
-    [SerializeField] private Transform _rightThigh;
-    
-    //Stand Pose
-    [SerializeField] private Transform _Sback;
-    [SerializeField] private Transform _SleftFoot;
-    [SerializeField] private Transform _SleftShin;
-    [SerializeField] private Transform _SleftThigh;
-    [SerializeField] private Transform _SrightFoot;
-    [SerializeField] private Transform _SrightShin;
-    [SerializeField] private Transform _SrightThigh; */
-    
+
+    private Vector3 _Ground;
+
+    private bool _hoverboardState = false;
     void Start()
     {
         _hoverboard.SetActive(false);
         _hoverBoardActivate.action.performed += ActivateHoverBoard;
+        _HoverUpDown.action.performed += UpDown;
 
+    }
+
+    private void UpDown(InputAction.CallbackContext obj)
+    {
+        float val = obj.ReadValue<float>();
+
+        if (_hoverboardState)
+        {
+            if (val > 0.95)
+            {
+                _XROrigin.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+                _Avatar.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            } else if (val < -0.95)
+            {
+                _XROrigin.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+                _Avatar.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+            }
+        }
         
     }
 
@@ -46,11 +58,14 @@ public class Hoverboard : MonoBehaviour
         if (_hoverboard.activeSelf)
         {
             _hoverboard.SetActive(false);
+            
             for (int i = 0; i < 7; i++)
             {
                 _bones[i].rotation = _standPose[i].rotation;
             }
-            
+
+            _hoverboardState = false;
+
         }
         else
         {
@@ -59,6 +74,8 @@ public class Hoverboard : MonoBehaviour
             {
                 _bones[i].rotation = _skatePose[i].rotation;
             }
+            
+            _hoverboardState = true;
         }
 
     }
@@ -66,6 +83,18 @@ public class Hoverboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_hoverboardState)
+        {
+            _XROriginTurn.enabled = false;
+            _XROriginMove.forwardSource = _forwardSource;
+        } 
         
+        else if (!_hoverboardState) 
+        {
+            _XROriginTurn.enabled = true;
+            _XROriginMove.forwardSource = _mainCamera;
+            
+            
+        }
     }
 }
