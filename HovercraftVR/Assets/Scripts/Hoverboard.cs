48d4ffd090dffa70f81123754d7de000543b8420
Lucaswang8 <hi@madebylucas.co>
2022-09-14 00:58:14 -0700

    Changed serialize field to arrays and got pose working on button press

--- HovercraftVR/Assets/Scripts/Hoverboard.cs
+++ HovercraftVR/Assets/Scripts/Hoverboard.cs
@@ -8,8 +8,12 @@ public class Hoverboard : MonoBehaviour
 {
     [SerializeField] private GameObject _hoverboard;
     [SerializeField] private InputActionReference _hoverBoardActivate;
+
+    [SerializeField] private Transform[] _bones = new Transform[7];
+    [SerializeField] private Transform[] _skatePose = new Transform[7];
+    [SerializeField] private Transform[] _standPose = new Transform[7];
     
-    //Save skating poses
+   /* //Save skating poses
     [SerializeField] private Transform _back;
     [SerializeField] private Transform _leftFoot;
     [SerializeField] private Transform _leftShin;
@@ -25,7 +29,7 @@ public class Hoverboard : MonoBehaviour
     [SerializeField] private Transform _SleftThigh;
     [SerializeField] private Transform _SrightFoot;
     [SerializeField] private Transform _SrightShin;
-    [SerializeField] private Transform _SrightThigh;
+    [SerializeField] private Transform _SrightThigh; */
     
     void Start()
     {
@@ -39,6 +43,11 @@ public class Hoverboard : MonoBehaviour
     {
         Debug.Log("Activate HoverBoard");
         _hoverboard.SetActive(true);
+
+        for (int i = 0; i < 7; i++)
+        {
+            _bones[i].rotation = _skatePose[i].rotation;
+        }
     }
 
     // Update is called once per frame
